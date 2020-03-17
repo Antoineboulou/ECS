9bb08ced1ab3cb77b54d701db1785f6e26ec059c
Antoineboulou <Antoineboulou@users.noreply.github.com>
2020-03-18 09:04:39 +1300

    Initial commit

---
+++ Assets/LevelUpSystem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class LevelUpSystem : ComponentSystem
+{
+    protected override void OnUpdate() {
+        Entities.ForEach((ref LevelUpComponent levelUpComponent) =>
+        {
+            levelUpComponent.level += 1f * Time.DeltaTime;
+            Debug.Log(levelUpComponent.level);
+
+        });
+    
+    }
+
+
+}
